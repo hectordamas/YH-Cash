fd8011dbcc5b27e34ccbf31fdfbf599b6fc899d8
Hector Damas <hectorgabrieldm@hotmail.com>
2024-03-09 19:52:33 -0400

    Implementacion de cierre de caja

--- app/Http/Controllers/ClosureController.php
+++ app/Http/Controllers/ClosureController.php
@@ -6,14 +6,38 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cash;
 use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 
 class ClosureController extends Controller
 {
+    public function verificarCierreCaja($closure_date) {
+        // Obtener la fecha actual
+        $fechaActual = Carbon::now();
+    
+        // Crear un CarbonPeriod para el mes anterior al mes actual
+        $inicioMesAnterior = $fechaActual->copy()->subMonthNoOverflow()->startOfMonth();
+        $finMesAnterior = $inicioMesAnterior->copy()->endOfMonth();
+        $periodoMesAnterior = CarbonPeriod::create($inicioMesAnterior, $finMesAnterior);
+    
+        // Verificar si la fecha de cierre de la caja se encuentra en el periodo del mes anterior al mes actual
+        if ($periodoMesAnterior->contains($closure_date)) {
+            // Si la fecha de cierre está en el periodo del mes anterior al mes actual, no permitir el cierre de caja
+            return false;
+        }
+    
+        // Si la fecha de cierre no está en el periodo del mes anterior al mes actual, permitir el cierre de caja si estamos dentro de los primeros 5 días del mes actual
+        return $fechaActual->day <= 5;
+    }
+
     public function create($id){
         $cash = Cash::find($id);
+        $puedeCerrarCaja = $this->verificarCierreCaja($cash->closure_date);
+        $fechaActual = Carbon::now();
 
         return view('cash.closure', [
-            'cash' => $cash
+            'cash' => $cash,
+            'puedeCerrarCaja' => $puedeCerrarCaja,
+            'fechaActual' => $fechaActual
         ]);
 
     }
--- resources/views/cash/closure.blade.php
+++ resources/views/cash/closure.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
@@ -16,11 +17,35 @@
                             <input type="date" value="{{ date('Y-m-d')}}" class="form-control" readonly > 
                         </div>
                     </div>
+
+                    @if($cash->closure_date)
+                        @if($puedeCerrarCaja)
+                        <div class="row">
+                            <div class="col-md-12 form-group">
+                                <input type="submit" class="btn btn-dark" value="Crear Cierre">
+                            </div>
+                        </div>
+                        @else
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="alert alert-danger">
+                                    @if($fechaActual->day > 5)
+                                        Actualmente no es posible cerrar la caja. Esto se debe a que el lapso de tiempo permitido para el cierre de la caja ha transcurrido. Nuestro sistema requiere que el cierre se realice dentro de los primeros 5 días del mes actual.                                     
+                                    @else
+                                        No se puede proceder con el cierre de la caja en este momento. El mes pasado ya ha sido cerrado, por lo que no es posible realizar un nuevo cierre para ese período.
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                    @else
                     <div class="row">
                         <div class="col-md-12 form-group">
-                            <input type="submit" class="btn btn-dark" value="Crear Cierre">
+                            <input type="submit" class="btn btn-dark" value="Crear Primer Cierre">
                         </div>
                     </div>
+                    @endif
+
                 </div>
             </form>
         </div>
